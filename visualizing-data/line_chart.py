7edd840fc5c65b61d38d5921ce2d6731c222d839
MAK <MAK@gmail.com>
2017-01-23 19:47:05 -1000

    add line chart

---
+++ visualizing-data/line_chart.py
@@ -0,0 +1,12 @@
+from matplotlib import pyplot as plt
+
+# plot performs numerical ordering of x values*
+days = [1,2,3,4,5,6,7]
+steps = [7074, 11722, 8282, 6086, 8373, 8417, 10471]
+
+plt.plot(days, steps, color='blue', marker='*', linestyle='--')
+plt.title('Weekly Steps')
+plt.ylabel('Steps')
+plt.xlabel('Days of the Week')
+# plt.show()
+plt.savefig('weekly_steps.png', format='png')
